71605312bf865b60119b24c1a3a2ea5a7823e22f
Sergio Henrique <sergiotjshg@gmail.com>
2023-08-14 22:36:37 -0300

    add new project api with healthcheck controller

---
+++ src/C.SocietyApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace C.SocietyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        // GET: api/<HealthCheckController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Im health";
+        }
+    }
+}
